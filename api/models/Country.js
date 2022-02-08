d3b8d34009de13266c34f093f49c53f5f762f07e
Casey Mc <casey@caseymcspadden.com>
2022-02-08 12:15:22 -0600

    Feb 8 Home

---
+++ api/models/Country.js
@@ -0,0 +1,53 @@
+/**
+ * Country.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    id: {
+      type: 'string',
+      maxLength: 10,
+      description: '2-letter country code',
+      example: 'US',
+      required: true
+    },
+
+    name: {
+      type: 'string',
+      maxLength: 100,
+      description: 'Country name',
+      required: true
+    },
+
+    description: {
+      type: 'string',
+      maxLength: 100,
+      description: 'Country description',
+      required: false
+    },
+
+    allowPayment: {
+      type: 'boolean',
+      description: 'Allow payments from this country',
+      defaultsTo: false
+    }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  }
+};
+
---
+++ api/models/State.js
@@ -0,0 +1,41 @@
+/**
+ * State.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+  
+    id: {
+      type: 'string',
+      maxLength: 10,
+      description: '2-letter state or province code',
+      example: 'KS',
+      required: true
+    },
+
+    name: {
+      type: 'string',
+      maxLength: 100,
+      description: 'State or province name',
+      required: true
+    },
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    country: {
+      model: 'Country',
+      required: true
+    }
+  }
+};
+
